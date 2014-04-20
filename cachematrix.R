11a662d3e3b472f6c3ebcc8603edccbcb3ec236e
Annie Flippo <acflippo@gmail.com>
2014-04-20 16:25:06 -0700

    added commits to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,12 +7,13 @@
 ## setinverse() will set the inverse of the matrix
 
 ## Annie Flippo - 4/16/2014  Initial Creation
+## Annie Flippo - 4/20/2014  Added comments
 
 makeCacheMatrix <- function(x = matrix()) {
         
         x <- x
         
-        ## Initialize inv_x (inverse of X) to NULL 
+        # Initialize inv_x (inverse of X) to NULL 
         inv_x <- NULL
         
         # Find number of rows and columns for x
@@ -24,21 +25,24 @@ makeCacheMatrix <- function(x = matrix()) {
                 stop("It is not a square matrix")
         } 
         
+	# Sets the x matrix with the argument passed in as y
+	# Sets the inv_x as a matrix of a NULL
         set <- function(y) {
                 x <<- y
-                
-                ## Sets the inv_x as a matrix of a NULL
                 inv_x <<- matrix() 
         }
 
+	# gets the original matrix 
         get <- function() {
                 x
         }
 
+        # Sets the inverse of the matrix with the argument passed in as matrixInverse
         setinverse <- function(matrixInverse) {
                inv_x <<- matrixInverse
         }
 
+        # Gets the inverse of the matrix, inv_x
         getinverse <- function() {
                 inv_x
         }
@@ -54,20 +58,23 @@ makeCacheMatrix <- function(x = matrix()) {
 ## otherwise it will calculate and set the inverse for the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Return a matrix that is the inverse of 'x'
         
-        ## Get the inverse of x
+        # Gets the inverse of x
         inv_x <- x$getinverse()
         
-        ## Inverse of matrix is NOT NULL
+        # Check to see if the inverse of matrix is NOT NULL
+	# If it's NOT NULL, return inv_x as retrieved above and ends cacheSolve function
         if(!is.null(inv_x)) {
                 message("getting cached data")
                 return(inv_x)
         }
         
-        ## Inverse of matrix is NULL so calculate and set it
+        # Inverse of matrix is NULL so calculate and set it
         data <- x$get()
         inv_x <- solve(data, ...)
         x$setinverse(inv_x)
+
+	# Returning inv_x
         inv_x
 }
